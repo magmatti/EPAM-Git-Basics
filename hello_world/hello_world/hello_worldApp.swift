99efa7bd44e7ad03efdbc57f9cdd842bdef889fb
Mateusz Wójtowicz <matiwojt@gmail.com>
2025-05-28 21:26:13 +0200

    add hello_world project

---
+++ hello_world/hello_world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello_world
+//
+//  Created by Mateusz Wójtowicz on 28/5/25.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
